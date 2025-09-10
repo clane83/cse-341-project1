46d9f70a6f6b3ff6583b67f0538d0e998563da44
clane83 <clane83@yahoo.com>
2025-09-09 18:57:57 -0700

    checkin swagger update

--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,7 @@ const mongodb = require('../data/database');
 const { ObjectId } = require('mongodb');
 
 const getAll = async (req, res) => {
+    //#swagger.tags['Users']
     const result = await mongodb.getDatabase().db().collection('users').find();
     result.toArray().then((users) => {
         res.setHeader('Content-Type', 'application/json');
@@ -10,6 +11,7 @@ const getAll = async (req, res) => {
 };
 
 const getSingle = async (req, res) => {
+    //#swagger.tags['Users']
     const userId = new ObjectId(req.params.id);
     const result = await mongodb.getDatabase().db().collection('users').find({ _id: userId });
     result.toArray().then((users) => {
@@ -19,7 +21,7 @@ const getSingle = async (req, res) => {
 };
 
 const createUser = async (req, res) => {
-    // Implementation for creating a user
+    //#swagger.tags['Users']
     const userId = new ObjectId(req.params.id);
     const user = {
         email: req.body.email,
@@ -36,7 +38,7 @@ const createUser = async (req, res) => {
 };
 
 const updateUser = async (req, res) => {
-    // Implementation for updating a user
+    //#swagger.tags['Users']
     const userId = new ObjectId(req.params.id);
     const user = {
         email: req.body.email,
@@ -53,7 +55,7 @@ const updateUser = async (req, res) => {
 };
 
 const deleteUser = async (req, res) => {
-    // Implementation for removing a user
+    //#swagger.tags['Users']
     const userId = new ObjectId(req.params.id);
     const response = await mongodb.getDatabase().db().collection('users').deleteOne({ _id: userId })
     if (response.deletedCount > 0) {
